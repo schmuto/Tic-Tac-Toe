bda5a11d0a1d1d9d8d9a0be6b3da4f1352a91fe0
Tobias S <schmuecker.tobias@gmail.com>
2015-03-08 00:12:26 +0100

    Added color selector (not working yet)

---
+++ SettingsViewController.swift
@@ -0,0 +1,61 @@
+//
+//  SettingsViewController.swift
+//  Tic Tac Toe
+//
+//  Created by Tobias Schmücker on 07.03.15.
+//  Copyright (c) 2015 Tobias S. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    
+    /*@IBOutlet weak var mainView: UIView!
+    
+    @IBAction func goBackBlue(sender: AnyObject) {
+        
+    }
+    
+    
+    @IBAction func goBackGreen(sender: AnyObject) {
+        
+    }
+    
+    
+    @IBAction func goBackOrange(sender: AnyObject) {
+        
+    }
+    
+    
+    @IBAction func goBackPink(sender: AnyObject) {
+        
+    }*/
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //var recognizer = UIGestureRecognizer(target: mainView, action: "unwindSegue:")
+        
+    }
+
+    /*func unwindSegue() {
+        println("unwind")
+    }*/
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
